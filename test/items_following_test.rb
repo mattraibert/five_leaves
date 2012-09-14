d81baa5d85412d1ab8fb0750b4d6c90b8a140f91
Matt Raibert <mattraibert@gmail.com>
2012-09-14 13:54:40 -0400

    add items following utility

---
+++ test/items_following_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'five_leaves/items_following'
+
+class ItemsFollowingTest < MiniTest::Unit::TestCase
+  def test_items_following
+    data = %w(I want text that follows this THESE TWO NOT THIS)
+    result = []
+    data.items_following(/this/) do |items|
+      result << items[0]
+      result << items[1]
+    end
+    assert result.include? "THESE"
+    assert result.include? "TWO"
+    assert !result.include?("NOT")
+    assert !result.include?("this")
+  end
+end
